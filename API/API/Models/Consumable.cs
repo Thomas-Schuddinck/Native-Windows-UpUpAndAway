a9f8ed0418d0237b5fdd1c15c0c7741e3c496d0d
thomas <thomas.schuddinck.y3378@student.hogent.be>
2020-06-24 22:39:13 +0200

    added class consumable

---
+++ API/API/Models/Consumable.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Models
+{
+    public class Consumable
+    {
+        #region Fields
+        private double _price;
+        private string _name;
+        private int _reduction = 0;
+        #endregion
+
+        #region Properties
+        public int ConsumableId { get; private set; }
+        public double Price
+        {
+            get
+            {
+                return _price;
+            }
+            private set
+            {
+                if (value < 0)
+                    throw new ArgumentException("the price cannot be negative!");
+            }
+        }
+        public string Name
+        {
+            get
+            {
+                return _name;
+            }
+            private set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                    throw new ArgumentException("The name cannot be empty or only containing whitespace!");
+                _name = value;
+
+            }
+        }
+        public string Description { get; private set; }
+        public int Reduction
+        {
+            get
+            {
+                return _reduction;
+            }
+            private set
+            {
+                if (value < 0 || value > 1)
+                    throw new ArgumentException("Price reduction cannot be lower than 0 and not be higher than 1!");
+                _reduction = value;
+
+            }
+        }
+
+        public double SellingPrice => _price * (1 - _reduction); 
+        #endregion
+
+        public Consumable(int consumableId, double price, string name, string description, int reduction)
+        {
+            ConsumableId = consumableId;
+            Price = price;
+            Name = name;
+            Description = description;
+            Reduction = reduction;
+        }
+    }
+}
