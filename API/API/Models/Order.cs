b7d2769e5cd12e90e6a563921a05dabd476e2571
thomas <thomas.schuddinck.y3378@student.hogent.be>
2020-06-24 22:59:56 +0200

    added classes orderline and order + added enum orderstatus

---
+++ API/API/Models/Order.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Models
+{
+    public class Order
+    {
+        #region Properties
+        public int OrderId { get; private set; }
+        public List<OrderLine> OrderLines { get; private set; }
+        public OrderStatus OrderStatus { get; private set; } 
+        #endregion
+
+        public Order(int orderId, List<OrderLine> orderLines, OrderStatus orderStatus)
+        {
+            OrderId = orderId;
+            OrderLines = orderLines;
+            OrderStatus = orderStatus;
+        }
+
+        public void SetAmountForOrderLine(OrderLine orderLine, int amount)
+        {
+            try
+            {
+                orderLine.SetNewAmount(amount);
+            }
+            catch (ArgumentException ex)
+            {
+                OrderLines.Remove(orderLine);
+            }
+        }
+
+        public void UpdateStatus(OrderStatus orderStatus)
+        {
+            OrderStatus++;
+            if (!(Enum.IsDefined(typeof(OrderStatus), OrderStatus)))
+            {
+                OrderStatus--;
+                throw new IndexOutOfRangeException();
+            }
+        }
+    }
+}
---
+++ API/API/Models/OrderLine.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Models
+{
+    public class OrderLine
+    {
+        #region Fields
+        private int _amount;
+        #endregion
+
+        #region Properties
+        public int OrderLineId { get; private set; }
+        public int Amount
+        {
+            get
+            {
+                return Amount;
+            }
+            private set
+            {
+                if (value <= 0)
+                    throw new ArgumentException("Amount cannot be zero or less!");
+                _amount = value;
+            }
+        }
+        public Consumable Consumable { get; private set; }
+        #endregion
+
+        public OrderLine(int orderLineId, int amount, Consumable consumable)
+        {
+            OrderLineId = orderLineId;
+            Amount = amount;
+            Consumable = consumable;
+        }
+
+        public void SetNewAmount(int value)
+        {
+            Amount =value;
+        }
+    }
+}
