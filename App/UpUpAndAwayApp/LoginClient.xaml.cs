a497638295c0f9811e18b4ef546b128ca0ab672b
Laurens Ghekiere <laurens.ghekiere@student.hogent.be>
2020-06-30 22:19:52 +0200

    Startup frontend + mainpage + Loginpage

--- App/UpUpAndAwayApp/LoginClient.xaml.cs
+++ App/UpUpAndAwayApp/LoginClient.xaml.cs
@@ -13,23 +13,23 @@ using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 
-// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
 
 namespace UpUpAndAwayApp
 {
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
-    public sealed partial class MainPage : Page
+    public sealed partial class LoginClient : Page
     {
-        public MainPage()
+        public LoginClient()
         {
             this.InitializeComponent();
         }
 
-        private void Button_Click(object sender, RoutedEventArgs e)
+        private void Logout_Click(object sender, RoutedEventArgs e)
         {
-            this.Frame.Navigate( typeof(LoginClient));
+            this.Frame.Navigate(typeof(MainPage));
         }
     }
 }
