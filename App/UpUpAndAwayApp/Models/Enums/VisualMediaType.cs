a8b6f03f6a2a50c9c27db9a7b8b1651cf8a2c7e6
thomas <thomas.schuddinck.y3378@student.hogent.be>
2020-07-01 15:58:54 +0200

    [UPDATE] updated enum namespaces

--- App/UpUpAndAwayApp/Models/Enums/VisualMediaType.cs
+++ App/UpUpAndAwayApp/Models/Enums/VisualMediaType.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace UpUpAndAwayApp.Models.Singleton
+namespace UpUpAndAwayApp.Models.Enums
 {
     public enum VisualMediaType
     {
