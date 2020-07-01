cff8a6a2c67d0c137eaab437ccbbdbb46250f6ff
thomas <thomas.schuddinck.y3378@student.hogent.be>
2020-07-01 15:47:09 +0200

    [ADD] VisualMediaType enum added

---
+++ App/UpUpAndAwayApp/Models/Enums/VisualMediaType.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UpUpAndAwayApp.Models.Singleton
+{
+    public enum VisualMediaType
+    {
+        Movie = 0,
+        Serie = 1,
+        Episode = 2
+    }
+}
