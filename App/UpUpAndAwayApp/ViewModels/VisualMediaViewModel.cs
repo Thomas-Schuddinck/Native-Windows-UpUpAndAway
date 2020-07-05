0fbffd267129d18b38b011cef84c28a2fca31da5
thomas <thomas.schuddinck.y3378@student.hogent.be>
2020-07-05 17:35:24 +0200

    [ADD] visual media viewmodel initialized

---
+++ App/UpUpAndAwayApp/ViewModels/VisualMediaViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using UpUpAndAwayApp.Models;
+namespace UpUpAndAwayApp.ViewModels
+{
+    public class VisualMediaViewModel
+    {
+        public IReadOnlyList<Movie> Movies { get; private set; }
+    }
+}
