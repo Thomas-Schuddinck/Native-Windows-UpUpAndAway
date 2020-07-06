176e90cab89141d9544b8787212fa9eb6239506b
thomas <thomas.schuddinck.y3378@student.hogent.be>
2020-07-06 15:21:20 +0200

    [ADD] api data + movie id's added

---
+++ App/UpUpAndAwayApp/Data/ApiData.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UpUpAndAwayApp.Data
+{
+    public static class ApiData
+    {
+        public static string apiKeyOMDB = "8f0aafb7";
+        public static string baseUriOMDB = $"http://www.omdbapi.com/?apikey={apiKeyOMDB}";
+    }
+}
---
+++ App/UpUpAndAwayApp/Data/VisualMediaData.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UpUpAndAwayApp.Data
+{
+    public static class VisualMediaData
+    {
+        // iron man, doctor strange, now you see me, now you see me 2, onward, wonder woman, alien, american pie, annabelle, the conjuring,
+        public static string[] movieIDs = { "tt0371746", "tt1211837", "tt1670345", "tt3110958", "tt7146812", "tt0451279", "tt0078748", "tt0163651", "tt3322940", "tt1457767" };
+        public static string[] serieIDs = { };
+    }
+}
