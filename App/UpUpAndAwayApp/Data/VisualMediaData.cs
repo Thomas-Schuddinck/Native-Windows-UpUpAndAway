3b6240f3a8b6cea988942c94105d41ffe925587b
thomas <thomas.schuddinck.y3378@student.hogent.be>
2020-07-06 17:31:37 +0200

    [UPDATE] added serie ID's

--- App/UpUpAndAwayApp/Data/VisualMediaData.cs
+++ App/UpUpAndAwayApp/Data/VisualMediaData.cs
@@ -10,6 +10,7 @@ namespace UpUpAndAwayApp.Data
     {
         // iron man, doctor strange, now you see me, now you see me 2, onward, wonder woman, alien, american pie, annabelle, the conjuring,
         public static string[] movieIDs = { "tt0371746", "tt1211837", "tt1670345", "tt3110958", "tt7146812", "tt0451279", "tt0078748", "tt0163651", "tt3322940", "tt1457767" };
-        public static string[] serieIDs = { };
+        // the flash, arrow, the magicians, got, sao, avater: tla, brooklyn nine-nine, friends, himym
+        public static string[] serieIDs = { "tt3107288", "tt2193021", "tt4254242", "tt0944947", "tt2250192", "tt0417299", "tt2467372", "tt0108778", "tt0460649" };
     }
 }
