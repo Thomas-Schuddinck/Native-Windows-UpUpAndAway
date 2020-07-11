322c4a3c084b4fa786b91cdc49533a26753657d2
thomas <thomas.schuddinck.y3378@student.hogent.be>
2020-07-11 18:41:05 +0200

    [UPDATE] added API-info for weather

--- App/UpUpAndAwayApp/Data/ApiData.cs
+++ App/UpUpAndAwayApp/Data/ApiData.cs
@@ -10,5 +10,7 @@ namespace UpUpAndAwayApp.Data
     {
         public static string apiKeyOMDB = "8f0aafb7";
         public static string baseUriOMDB = $"http://www.omdbapi.com/?apikey={apiKeyOMDB}";
+        public static string apiKeyOWM = "4ba9350345b18472273788211e2bf691";
+        public static string baseUriOWM = $"http://api.openweathermap.org";
     }
 }
