283761d7db5c274bac4ec022cc0bae001953ca56
thomas <thomas.schuddinck.y3378@student.hogent.be>
2020-07-11 18:43:15 +0200

    [ADD] FlightInfoViewModel added

---
+++ App/UpUpAndAwayApp/ViewModels/FlightInfoViewModel.cs
@@ -0,0 +1,32 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Net.Http;
+using UpUpAndAwayApp.Data;
+using UpUpAndAwayApp.Models;
+
+namespace UpUpAndAwayApp.ViewModels
+{
+    public class FlightInfoViewModel
+    {
+        public OpenWeatherMapResponse Weather { get; private set; }
+
+        public FlightInfoViewModel()
+        {
+            getWeather();
+        }
+
+        public async void getWeather()
+        {
+            string city = "Los Angeles";
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(ApiData.baseUriOWM);
+                var response = await client.GetAsync($"/data/2.5/weather?q={city}&appid={ApiData.apiKeyOWM}&units=metric");
+
+                var jsonResponse = await response.Content.ReadAsStringAsync();
+                Weather = JsonConvert.DeserializeObject<OpenWeatherMapResponse>(jsonResponse);
+
+            }
+        }
+    }
+}
