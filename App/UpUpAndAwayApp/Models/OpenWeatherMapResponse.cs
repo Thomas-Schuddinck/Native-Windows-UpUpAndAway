dc256df460495e752a1f2174298033a00d2666b0
thomas <thomas.schuddinck.y3378@student.hogent.be>
2020-07-11 18:42:48 +0200

    [ADD] added weather DTO

---
+++ App/UpUpAndAwayApp/Models/OpenWeatherMapResponse.cs
@@ -0,0 +1,28 @@
+﻿namespace UpUpAndAwayApp.Models
+{
+    public class OpenWeatherMapResponse
+    {
+        public WeatherDescription Weather { get; set; }
+
+        public Main Main { get; set; }
+
+        public Main Wind { get; set; }
+    }
+
+    public class WeatherDescription
+    {
+        public string Main { get; set; }
+        public string Description { get; set; }
+    }
+
+    public class Main
+    {
+        public string Temp { get; set; }
+        public string Humidity { get; set; }
+    }
+
+    public class Wind
+    {
+        public string Speed { get; set; }
+    }
+}
