acb3a5c506f40e8399bf1ea030e4fbce92e833b3
thomas <thomas.schuddinck.y3378@student.hogent.be>
2020-07-16 03:00:50 +0200

    [UPDATE] visualmedia VM replace media ID's to names

--- App/UpUpAndAwayApp/ViewModels/VisualMediaViewModel.cs
+++ App/UpUpAndAwayApp/ViewModels/VisualMediaViewModel.cs
@@ -26,7 +26,7 @@ namespace UpUpAndAwayApp.ViewModels
         private async void GetMoviesFromAPI()
         {
             HttpClient client = new HttpClient();
-            foreach(string id in VisualMediaData.movieIDs)
+            foreach(string id in VisualMediaData.movieTitels)
             {
                 var jsonResponse = await client.GetStringAsync(new Uri(GenerateMovieRequestString(id)));
                 var movie = JsonConvert.DeserializeObject<Movie>(jsonResponse);
@@ -37,22 +37,22 @@ namespace UpUpAndAwayApp.ViewModels
         private async void GetSeriesFromAPI()
         {
             HttpClient client = new HttpClient();
-            foreach (string id in VisualMediaData.serieIDs)
+            foreach (string title in VisualMediaData.serieTitels)
             {
-                var jsonResponse = await client.GetStringAsync(new Uri(GenerateMovieRequestString(id)));
+                var jsonResponse = await client.GetStringAsync(new Uri(GenerateSerieRequestString(title)));
                 var serie = JsonConvert.DeserializeObject<Serie>(jsonResponse);
                 Series.Add(serie);
             }
         }
 
-        private string GenerateMovieRequestString(string movieId)
+        private string GenerateMovieRequestString(string movieTitle)
         {
-            return String.Format("{0}&i={1}&type=movie", ApiData.baseUriOMDB, movieId);
+            return String.Format("{0}&t={1}&type=movie&plot=full", ApiData.baseUriOMDB, movieTitle);
         }
 
-        private string GenerateSerieRequestString(string serieId)
+        private string GenerateSerieRequestString(string serieTitle)
         {
-            return String.Format("{0}&i={1}&type=series", ApiData.baseUriOMDB, serieId);
+            return String.Format("{0}&t={1}&type=series&plot=full", ApiData.baseUriOMDB, serieTitle);
         }
     }
 }
