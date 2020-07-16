7dfc09a88d81421e80532cd30a0e00aead17dcbb
thomas <thomas.schuddinck.y3378@student.hogent.be>
2020-07-16 17:14:42 +0200

    [ADD] webshop VM added

---
+++ App/UpUpAndAwayApp/ViewModels/WebshopViewModel.cs
@@ -0,0 +1,19 @@
+﻿using API.Models;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UpUpAndAwayApp.ViewModels
+{
+    public class WebshopViewModel
+    {
+        public ObservableCollection<Consumable> Consumables { get; private set; }
+
+        public WebshopViewModel()
+        {
+        }
+    }
+}
