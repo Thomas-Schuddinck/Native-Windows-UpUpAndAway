d2894083ca0b0820940ab4db1291bd7cf1f92053
thomas <thomas.schuddinck.y3378@student.hogent.be>
2020-07-19 21:37:09 +0200

    [UPDATE] small model changes

--- App/UpUpAndAwayApp/Models/Consumable.cs
+++ App/UpUpAndAwayApp/Models/Consumable.cs
@@ -1,27 +1,27 @@
 ﻿using System;
 
-namespace API.Models
+namespace UpUpAndAwayApp.Models
 {
     public class Consumable
     {
         #region Properties
+        
         public int ConsumableId { get; set; }
         public double Price { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
         public string ProductPicture { get; set; }
         public int Reduction { get; set; }
-
-        public double SellingPrice => Price * (1 - Reduction); 
+        public double SellingPrice { get; set; }
         #endregion
 
-        public Consumable(double price, string name, string description, int reduction, string productPicture)
+        public Consumable(double price, string name, string description, int reduction, double sellingPrice)
         {
             Price = price;
             Name = name;
             Description = description;
             Reduction = reduction;
-            ProductPicture = productPicture;
+            SellingPrice = sellingPrice;
         }
     }
 }
--- App/UpUpAndAwayApp/Models/OrderLine.cs
+++ App/UpUpAndAwayApp/Models/OrderLine.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 
-namespace API.Models
+namespace UpUpAndAwayApp.Models
 {
     public class OrderLine
     {
