f760d521cabcfc0243abf171093961c2eff9f19d
thomas <thomas.schuddinck.y3378@student.hogent.be>
2020-07-19 21:38:00 +0200

    [ADD] added "add to cart" functionality

--- App/UpUpAndAwayApp/Pages/Webshop.xaml.cs
+++ App/UpUpAndAwayApp/Pages/Webshop.xaml.cs
@@ -1,18 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
+﻿using UpUpAndAwayApp.Models;
+using UpUpAndAwayApp.Models.ListItemModels;
 using UpUpAndAwayApp.ViewModels;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
-using Windows.UI.Xaml.Navigation;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
 
@@ -29,5 +19,15 @@ namespace UpUpAndAwayApp.Pages
             this.ViewModel = new WebshopViewModel();
         }
         public WebshopViewModel ViewModel;
+
+        public void SendToShoppingCart(object sender, RoutedEventArgs e)
+        {
+            var button = (Button)sender;
+            var item = (WebshopItem)button.DataContext;
+            ViewModel.AddToShoppingCart(item);
+        }
     }
+
+
+
 }
