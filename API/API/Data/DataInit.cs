131442e9c9b99b892626bce344d931ff43b9e193
Nick Lersberghe <nick.lersberghe@student.hogent.be>
2020-07-20 22:41:57 +0200

    Fixed errors and code thats waay to long in DataInit

--- API/API/Data/DataInit.cs
+++ API/API/Data/DataInit.cs
@@ -21,25 +21,52 @@ namespace API.Data
             context.Database.EnsureDeleted();
             if (context.Database.EnsureCreated())
             {
-                Consumable burger = new Consumable(16.5, "Burger", "Een heerlijke burger op grootmoeder's wijze", 0);
-                Consumable spaghetti = new Consumable(14.5, "Spaghetti Bolognaise", "Een echte Italiaanse spaghetti", 0);
-                Consumable cola = new Consumable(3.5, "Cola", "Een frisse Coca Cola", 0);
-                Consumable colaZero = new Consumable(3.5, "Cola Zero", "Een frisse Coca Cola Zero", 0);
-                Consumable fanta = new Consumable(4, "Fanta", "Een frisse Fanta", 0);
-                Consumable jupiler = new Consumable(5, "Jupiler", "Een verkoelende Jupiler", 0);
-                Consumable chipsPaprika = new Consumable(3, "Paprika Chips", "Paprika chips van Lays", 0);
-                Consumable chipsZout = new Consumable(3, "Gezouten Chips", "Gezouten chips van Lays", 0);
 
-                context.Consumables.Add(burger);
-                context.Consumables.Add(spaghetti);
-                context.Consumables.Add(cola);
-                context.Consumables.Add(fanta);
-                context.Consumables.Add(colaZero);
-                context.Consumables.Add(jupiler);
-                context.Consumables.Add(chipsPaprika);
-                context.Consumables.Add(chipsZout);
-                context.SaveChanges();
+                SeedData();
+               
+
             }
+
+        }
+
+        private void SeedData()
+        {
+            context.Consumables.AddRange(new[] {
+                    new Consumable(16.5, "Burger", "Een heerlijke burger op grootmoeder's wijze", 0, ""),
+                    new Consumable(14.5, "Spaghetti Bolognaise", "Een echte Italiaanse spaghetti", 0, ""),
+                    new Consumable(3.5, "Cola", "Een frisse Coca Cola", 0, ""),
+                    new Consumable(3.5, "Cola Zero", "Een frisse Coca Cola Zero", 0, ""),
+                    new Consumable(4, "Fanta", "Een frisse Fanta", 0, ""),
+                    new Consumable(5, "Jupiler", "Een verkoelende Jupiler", 0, ""),
+                    new Consumable(3, "Paprika Chips", "Paprika chips van Lays", 0, ""),
+                    new Consumable(3, "Gezouten Chips", "Gezouten chips van Lays", 0, "")
+                });
+
+            context.SaveChanges();
+            //Herschreven zodat het op 2 lijntjes past
         }
     }
 }
+
+#region old code
+/*
+//Consumable burger = new Consumable(16.5, "Burger", "Een heerlijke burger op grootmoeder's wijze", 0, "");
+//Consumable spaghetti = new Consumable(14.5, "Spaghetti Bolognaise", "Een echte Italiaanse spaghetti", 0, "");
+//Consumable cola = new Consumable(3.5, "Cola", "Een frisse Coca Cola", 0, "");
+//Consumable colaZero = new Consumable(3.5, "Cola Zero", "Een frisse Coca Cola Zero", 0, "");
+//Consumable fanta = new Consumable(4, "Fanta", "Een frisse Fanta", 0, "");
+//Consumable jupiler = new Consumable(5, "Jupiler", "Een verkoelende Jupiler", 0, "");
+//Consumable chipsPaprika = new Consumable(3, "Paprika Chips", "Paprika chips van Lays", 0, "");
+//Consumable chipsZout = new Consumable(3, "Gezouten Chips", "Gezouten chips van Lays", 0, "");
+
+//context.Consumables.Add(burger);
+//context.Consumables.Add(spaghetti);
+//context.Consumables.Add(cola);
+//context.Consumables.Add(fanta);
+//context.Consumables.Add(colaZero);
+//context.Consumables.Add(jupiler);
+//context.Consumables.Add(chipsPaprika);
+//context.Consumables.Add(chipsZout);
+//context.SaveChanges();
+*/
+#endregion
