f95d093831b359cf618ed35de331ead4fe3af995
thomas <thomas.schuddinck.y3378@student.hogent.be>
2020-08-08 00:59:09 +0200

    [STYLEFIX] fix voor right alignment inputfields

--- API/API/Data/DataInit.cs
+++ API/API/Data/DataInit.cs
@@ -14,7 +14,7 @@ namespace API.Data
 
         public async Task InitializeData()
         {
-            //context.Database.EnsureDeleted();
+            context.Database.EnsureDeleted();
             if (context.Database.EnsureCreated())
             {
                 SeedData();
